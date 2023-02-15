53ad1208099675a47687918e803fa47ba63e2cc8
adrian-parra <adrian.adrianborquez@hotmail.com>
2023-02-14 21:06:23 -0700

    add controller , model confirmar_correo
    Se agrego controller confirmar_correo con sus metodos basicos de una api

---
+++ app/Http/Controllers/ConfirmarCorreoController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\confirmar_correo;
+use Illuminate\Http\Request;
+
+class ConfirmarCorreoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\confirmar_correo  $confirmar_correo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(confirmar_correo $confirmar_correo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\confirmar_correo  $confirmar_correo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, confirmar_correo $confirmar_correo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\confirmar_correo  $confirmar_correo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(confirmar_correo $confirmar_correo)
+    {
+        //
+    }
+}
