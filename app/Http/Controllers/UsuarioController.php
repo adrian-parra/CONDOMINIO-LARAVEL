c7e7a53ddd902dcdb3ecf99b461de058aa384a7e
adrian-parra <adrian.adrianborquez@hotmail.com>
2023-02-14 21:04:57 -0700

    add controller ,model usuarios
    se agrego controller usuario con metodos comunes de api

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\usuario;
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\usuario  $usuario
+     * @return \Illuminate\Http\Response
+     */
+    public function show(usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\usuario  $usuario
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\usuario  $usuario
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(usuario $usuario)
+    {
+        //
+    }
+}
