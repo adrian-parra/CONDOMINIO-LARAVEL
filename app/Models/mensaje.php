4aad4280e0e9e26c89504677d15716cb4ef1583b
adrian-parra <adrian.adrianborquez@hotmail.com>
2023-02-18 15:34:34 -0700

    add model mensaje
    Este modelo sera usado por todas las respuestas de la api que su metodo sea POST ,PUT O DELETE

---
+++ app/Models/mensaje.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class mensaje extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'icon',
+        'title',
+        'body'
+    ];
+}
