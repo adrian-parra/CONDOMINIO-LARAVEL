1136f8fdfc58276bdd30b140597632f6f5e40382
HectorSauR <78789693+HectorSauR@users.noreply.github.com>
2023-02-22 18:10:16 -0700

    Versionando Proyecto

--- app/Http/Controllers/Api/V1/ConfirmarCorreoController.php
+++ app/Http/Controllers/Api/V1/ConfirmarCorreoController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
 use App\Mail\confirmarRegistroFraccionamiento;
 use App\Models\confirmar_correo;
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,9 @@
 <?php
 
-use App\Http\Controllers\ConfirmarCorreoController;
-use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\Api\V1\ConfirmarCorreoController;
+use App\Http\Controllers\Api\V1\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UsersController;
-use App\Http\Controllers\JWTController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,27 +23,32 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 /**
-* ! PRUEBAS CON RUTAS DE LA API
-* Route::post('generate-token', [JWTController::class,'generateToken']);
-* Route::post('validate-token', [JWTController::class,'validateToken']);
-* Route::post('user/login', [JWTController::class,'login']); 
-*/
+ * ! PRUEBAS CON RUTAS DE LA API
+ * Route::post('generate-token', [JWTController::class,'generateToken']);
+ * Route::post('validate-token', [JWTController::class,'validateToken']);
+ * Route::post('user/login', [JWTController::class,'login']); 
+ */
 
 
 /**
  * MIDDLEWARE PARA AUTENTICACION DE TOKEN DE ACCESO JWT
  * PARA RUTAS PROTEGIDAS
  */
-Route::middleware(['jwt.auth'])->group(function () {
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'middleware' => 'jwt.auth'], function () {
     // Protected routes
-    Route::apiResource('usuario',UsuarioController::class);
+    Route::apiResource('usuario', UsuarioController::class);
+    // Route::apiResource('propietario', PropietarioController::class);
+    // Route::apiResource('propiedad', Propiedad::class);
 
 });
 
-Route::post('usuario/confirmar-registro' ,[ConfirmarCorreoController::class ,'confirmarRegistroFraccionamiento']);
-Route::post("usuario/confirmar-registro/check-token" , [ConfirmarCorreoController::class ,'checkTokenRegistroFraccionamiento']);
-Route::post("usuario/registrar" ,[UsuarioController::class ,'store']);
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+    // Debug Routes
+    Route::apiResource('propietario', PropietarioController::class);
+    Route::apiResource('propiedad', Propiedad::class);
+});
 
-Route::post("usuario/iniciar-sesion" ,[UsuarioController::class ,'iniciarSesion']);
-
-
+Route::post('usuario/confirmar-registro', [ConfirmarCorreoController::class, 'confirmarRegistroFraccionamiento']);
+Route::post("usuario/confirmar-registro/check-token", [ConfirmarCorreoControllereoController::class, 'checkTokenRegistroFraccionamiento']);
+Route::post("usuario/registrar", [UsuarioController::class, 'store']);
+Route::post("usuario/iniciar-sesion", [UsuarioController::class, 'iniciarSesion']);
