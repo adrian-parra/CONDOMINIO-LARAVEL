00360c56f1ebc3707ca878afe15651857a0e7d09
adrian-parra <adrian.adrianborquez@hotmail.com>
2023-02-28 22:17:59 -0700

    feat vehiculos
    Se creo crud

---
+++ app/Http/Controllers/Api/V1/VehiculoController.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Requests\V1\Vehiculo\StoreVehiculoRequest;
+use App\Http\Requests\V1\Vehiculo\UpdateVehiculoRequest;
+use App\Models\EstadosDeMexico;
+use App\Models\mensaje;
+use App\Models\TipoVehiculo;
+use App\Models\Vehiculo;
+use App\Utils\AlmacenarArchivo;
+use App\Utils\EliminarArchivo;
+use Illuminate\Http\Request;
+
+class VehiculoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        //
+        $mensaje = new mensaje();
+        $ConfigurarPagos = Vehiculo::where('id_fraccionamiento', $request->id_fraccionamiento)->get();
+        $mensaje->title = "Configuracion de pagos obtenidos";
+        $mensaje->icon = "success";
+        $mensaje->body = $ConfigurarPagos;
+        
+        return response()->json($mensaje, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    public function getEstadosMexico(){
+        $estados = EstadosDeMexico::all();
+
+        return response()->json([
+            'estados' => $estados
+        ] ,200);
+    }
+
+    public function getTiposVehiculos(){
+        $estados = TipoVehiculo::all();
+
+        return response()->json([
+            'tipos_vehiculos' => $estados
+        ] ,200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreVehiculoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreVehiculoRequest $request)
+    {
+        $mensaje = new mensaje();
+
+         //GUARDAR DATOS DEL VEHICULO
+         $vehiculo = new Vehiculo();
+         $vehiculo->id_propiedad = $request->id_propiedad;
+         $vehiculo->id_fraccionamiento = $request->id_fraccionamiento;
+         $vehiculo->id_tipo_vehiculo = $request->id_tipo_vehiculo;
+         $vehiculo->id_estado = $request->id_estado_emisor_placas;
+         $vehiculo->marca = $request->marca;
+         $vehiculo->color = $request->color;
+         $vehiculo->placas = $request->placas;
+         $vehiculo->estatus = true;
+
+        /**
+         * ? VERIFICAR SI ARCHIVO TARJETA DE CIRCULACION SE HA RECIBIDO
+         */
+        if($request->hasFile('tarjeta_circulacion')){
+            $file = $request->file('tarjeta_circulacion');
+
+            //ALMACENO ARCHIVO EN RUTA PRIVATE
+            $almacenarArchivo = new AlmacenarArchivo($file ,'private/tarjetasCirculacion');
+            $path = $almacenarArchivo->storeFile();
+
+            //REGISTRO PATCH DE DONDE SE GUARDO EL ARCHIVO
+            $vehiculo->patch_tarjeta_circulacion = $path;
+
+        }else{
+            $vehiculo->patch_tarjeta_circulacion = null;
+        }
+
+        $vehiculo->save();
+
+        $mensaje->title ="Datos almacenados";
+        $mensaje->icon ="success";
+        return response()->json($mensaje ,200);
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Vehiculo  $vehiculo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vehiculo $vehiculo)
+    {
+        //
+        $mensaje = new mensaje();
+        try {
+            //$vehiculo =$vehiculo::find(request()->vehiculo);
+            $mensaje->title = "Vehiculo obtenido";
+            $mensaje->icon = "success";
+            $mensaje->body = $vehiculo;
+            return response()->json($mensaje, 200);
+        }catch(\Exception $e){
+            $mensaje->title = "Esta peticion no se pudo completar";
+            $mensaje->icon = "error";
+            return response()->json($mensaje, 422);
+         
+        }
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Vehiculo  $vehiculo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vehiculo $vehiculo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateVehiculoRequest  $request
+     * @param  \App\Models\Vehiculo  $vehiculo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateVehiculoRequest $request, Vehiculo $vehiculo)
+    {
+        //
+        $mensaje = new mensaje();
+
+
+        $vehiculo->id_tipo_vehiculo = $request->id_tipo_vehiculo;
+        $vehiculo->id_estado = $request->id_estado_emisor_placas;
+        $vehiculo->marca = $request->marca;
+        $vehiculo->color =$request->color;
+        $vehiculo->placas =$request->placas;
+
+        if($request->hasFile('tarjeta_circulacion')){
+            //SI RECIBO UN ARCHIVO EN TARJETA DE CIRCULACION
+            //ELIMINO EL ARCHIVO ANTERIOR SI EXISTE ALGUNO
+
+            if($vehiculo->patch_tarjeta_circulacion !== null){
+                $deleteFile = new EliminarArchivo($vehiculo->patch_tarjeta_circulacion);
+                $deleteFile->deletePrivateFile();
+            }
+            
+            $file = $request->file('tarjeta_circulacion');
+
+            //ALMACENO ARCHIVO EN RUTA PRIVATE
+            $almacenarArchivo = new AlmacenarArchivo($file ,'private/tarjetasCirculacion');
+            $path = $almacenarArchivo->storeFile();
+
+            //REGISTRO PATCH DE DONDE SE GUARDO EL ARCHIVO
+            $vehiculo->patch_tarjeta_circulacion = $path;
+        }
+
+        $vehiculo->update();
+
+        $mensaje->title ="Datos actualizados";
+        $mensaje->icon ="success";
+        return response()->json($mensaje ,200);
+        
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Vehiculo  $vehiculo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vehiculo $vehiculo)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/V1/Vehiculo/StoreVehiculoRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests\V1\Vehiculo;
+
+use App\Models\mensaje;
+use Illuminate\Foundation\Http\FormRequest;
+
+use Illuminate\Validation\Rule;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class StoreVehiculoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+             $mensaje = new mensaje();
+            $mensaje->body = $validator->errors();
+            $mensaje->title = "error";
+            $mensaje->icon = "error";
+        
+        throw new HttpResponseException(
+            new JsonResponse(
+               $mensaje
+            , 422)
+        );
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            //
+            'id_fraccionamiento' => 'required',
+            'id_propiedad' => 'required',
+            'id_tipo_vehiculo' => 'required',
+            'id_estado_emisor_placas' => 'required',
+            'marca'=>'required',
+            'color' => 'required',
+            'placas' => 'required',
+        ];
+    }
+}
